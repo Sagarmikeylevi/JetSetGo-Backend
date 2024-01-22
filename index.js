4144259d8ab2c6b98a20923bb4c855203f5cb34f
unknown <sagarmikeylevi@gmail.com>
2024-01-22 20:27:05 +0530

    changed the cors options

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ module.exports.razorpayInstance = new Razorpay({
   key_secret: process.env.RAZORPAY_API_SECRECT,
 });
 
-app.use(cors());
+const allowedOrigins = ["http://localhost:5173"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
