d06574c824829ab0f7fb88c37bfd8f348a2df6b1
unknown <sagarmikeylevi@gmail.com>
2024-01-22 22:04:14 +0530

    ...

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -3,6 +3,8 @@ const crypto = require("crypto");
 require("dotenv").config();
 const Payment = require("../models/Payment");
 
+const TIMEOUT = 3000; // 3 seconds
+
 module.exports.createOrder = async (req, res) => {
   let { totalAmmount } = req.body;
   totalAmmount = Number(totalAmmount * 100);
@@ -11,14 +13,18 @@ module.exports.createOrder = async (req, res) => {
     amount: totalAmmount,
     currency: "INR",
   };
-  const order = await razorpayInstance.orders.create(options);
 
-  //   console.log(order);
+  // Wrap the creation of the order in a setTimeout
+  setTimeout(async () => {
+    const order = await razorpayInstance.orders.create(options);
 
-  res.status(200).json({
-    success: true,
-    order,
-  });
+    // console.log(order);
+
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  }, TIMEOUT);
 };
 
 module.exports.paymentVarification = async (req, res) => {
@@ -31,25 +37,31 @@ module.exports.paymentVarification = async (req, res) => {
 
   let isSignatureValid = generatedSignature === razorpay_signature;
 
-  if (isSignatureValid) {
-    await Payment.create({
-      razorpay_payment_id,
-      razorpay_order_id,
-      razorpay_signature,
-    });
-    res.redirect(
-      `http://localhost:5173/paymentsuccess?paymentId=${razorpay_payment_id}`
-    );
-  } else {
-    res.status(400).json({
-      success: false,
-      message: "Signature verification failed",
-    });
-  }
+  // Wrap the payment verification logic in a setTimeout
+  setTimeout(async () => {
+    if (isSignatureValid) {
+      await Payment.create({
+        razorpay_payment_id,
+        razorpay_order_id,
+        razorpay_signature,
+      });
+      res.redirect(
+        `http://localhost:5173/paymentsuccess?paymentId=${razorpay_payment_id}`
+      );
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "Signature verification failed",
+      });
+    }
+  }, TIMEOUT);
 };
 
 module.exports.getRazorpayAPIKEY = async (req, res) => {
-  res.status(200).json({
-    apiKey: process.env.RAZORPAY_API_KEY,
-  });
+  // Wrap the response in a setTimeout
+  setTimeout(() => {
+    res.status(200).json({
+      apiKey: process.env.RAZORPAY_API_KEY,
+    });
+  }, TIMEOUT);
 };
